5d7cf225e1353b3c54eaf2c4c9d8489d0eac0ce4
barbosa <eurisonfbarbosa@gmail.com>
2022-10-13 21:32:34 -0300

    creater storybook-static

---
+++ storybook-static/assets/preview.28f9ac49.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.4cba3679.js";import"./iframe.1fb09326.js";import"./es.map.constructor.d477613f.js";import"./es.number.to-fixed.9cefe100.js";import"./index.286ccdc0.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.28f9ac49.js.map
